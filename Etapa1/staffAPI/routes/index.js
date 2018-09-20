da61a92661a218a398e6a5d9f3ac95755a05cdbb
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-09-20 14:50:27 -0300

    Routes and queries for Employees

--- Etapa1/staffAPI/routes/index.js
+++ Etapa1/staffAPI/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Staff API' });
 });
+
+// Employee endpoints
+router.get('/api/employees', db.getAllEmployees);
+router.get('/api/employees/:id', db.getSingleEmployee);
+router.post('/api/employees', db.createEmployee);
+router.put('/api/employees/:id', db.updateEmployee);
+router.delete('/api/employees/:id', db.removeEmployee);
 
 module.exports = router;
