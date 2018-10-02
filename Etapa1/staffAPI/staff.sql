eda7288bec7213a8ee8b83ba4e00b61e829d1439
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-10-02 19:15:34 -0300

    staff bd added rows

--- Etapa1/staffAPI/staff.sql
+++ Etapa1/staffAPI/staff.sql
@@ -19,7 +19,15 @@ CREATE TABLE employees (
 );
 
 INSERT INTO employeeTypes (initials, description)
-  VALUES ('ca', 'cajero');
+  VALUES ('ca', 'cajero'), ('ge', 'gerente'), ('po', 'portero');
 
-INSERT INTO employees (firstname, surname, email, password, employeetype)
-  VALUES ('Juan', 'Perez', 'juanperez@gmail.com', 'juanperez123', 1);
+INSERT INTO employees (firstname, surname, email, password, employeetype) VALUES 
+  ('Juan', 'Perez', 'juanperez@gmail.com', 'juanperez123', 1),
+  ('Pedro', 'Perez', 'pedroperez@gmail.com', 'pedroperez123', 1),
+  ('Jose', 'Gallardo', 'josegallardo@gmail.com', 'josegallardo123', 2),
+  ('Manuel', 'Arias', 'manuelarias@gmail.com', 'manuelarias123', 2),
+  ('Andres', 'Carmona', 'andrescarmona@gmail.com', 'andrescarmona123', 3),
+  ('Julia', 'Vargas', 'juliavargas@gmail.com', 'juliavargas123', 1),
+  ('Daniela', 'Mora', 'danielamora@gmail.com', 'danielamora123', 2),
+  ('Marta', 'Sanchez', 'martasanchez@gmail.com', 'martasanchez123', 3),
+  ('Cristina', 'Moya', 'cristinamoya@gmail.com', 'cristinamoya123', 3);
