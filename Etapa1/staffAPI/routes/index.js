414c3d9ac5cee697d9923e3a1086967ee7b5262d
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-10-02 22:01:47 -0300

    Removed crud paths for employees

--- Etapa1/staffAPI/routes/index.js
+++ Etapa1/staffAPI/routes/index.js
@@ -10,16 +10,16 @@ router.get('/', function(req, res, next) {
 // Employee endpoints
 router.get('/api/employees', db.getAllEmployees);
 router.get('/api/employees/:id', db.getSingleEmployee);
-router.post('/api/employees', db.createEmployee);
-router.put('/api/employees/:id', db.updateEmployee);
-router.delete('/api/employees/:id', db.removeEmployee);
+//router.post('/api/employees', db.createEmployee);
+//router.put('/api/employees/:id', db.updateEmployee);
+//router.delete('/api/employees/:id', db.removeEmployee);
 
 // EmployeeTypes endpoints
-router.get('/api/employeeTypes', db.getAllEmployeeTypes);
-router.get('/api/employeeTypes/:id', db.getSingleEmployeeType);
-router.post('/api/employeeTypes', db.createEmployeeType);
-router.put('/api/employeeTypes/:id', db.updateEmployeeType);
-router.delete('/api/employeeTypes/:id', db.removeEmployeeType);
+//router.get('/api/employeeTypes', db.getAllEmployeeTypes);
+//router.get('/api/employeeTypes/:id', db.getSingleEmployeeType);
+//router.post('/api/employeeTypes', db.createEmployeeType);
+//router.put('/api/employeeTypes/:id', db.updateEmployeeType);
+//router.delete('/api/employeeTypes/:id', db.removeEmployeeType);
 
 // Project Endpoints
 router.get('/api/isEmployee/:email', db.isEmployee);
