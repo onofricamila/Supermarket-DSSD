17783a40c3cdd30c6df2f14b959d5bce9087c4b1
Franco Liptak <franco.liptak@despegar.com>
2018-11-10 16:53:48 -0300

    bases lista de productos

--- Etapa3/supermarket-front/src/App.js
+++ Etapa3/supermarket-front/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Footer from '../src/components/footer/footer';
 import Header from '../src/components/header/header';
 import Login from '../src/components/login/login';
-import Product from '../src/components/product/product';
+import ProductList from '../src/components/products-list/products-list'
 import './App.css';
 
 class App extends Component {
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Login />
-        <Product />
+        <ProductList />
         <Footer />
       </div>
     );
---
+++ Etapa3/supermarket-front/src/components/products-list/products-list.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Product from '../product/product';
+import './products-list.css';
+
+const productsList = props => (
+    <div className="products-list">
+        <div className="row">
+            <div className="col-sm-12 col-md-4">
+                <Product />
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <Product />
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <Product />
+            </div>
+        </div>
+    </div>
+);
+
+
+export default productsList;
