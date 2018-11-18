1afd2d714eb5961ae97ed6d5cc896a29feb1b45e
Franco Liptak <franco.liptak@despegar.com>
2018-11-17 22:38:28 -0300

    se cambia class por className

--- Etapa3/supermarket-front/src/components/product/product.jsx
+++ Etapa3/supermarket-front/src/components/product/product.jsx
@@ -7,7 +7,7 @@ const product = props => (
             <h3 className="card-title capitalize">{props.name}</h3>
             <h6 className="card-subtitle mb-2 text-muted capitalize">{props.type}</h6>
             <p className="card-text product-price-size">$ {props.price}</p>
-            <a href="#" class="btn btn-primary">Comprar</a>
+            <a href="#" className="btn btn-primary">Comprar</a>
         </div>
     </div>
 );
