c40f914a1670b6145d52b2bb255e8d2340015637
Camila Onofri <onofricamila2@gmail.com>
2018-11-20 13:39:03 -0300

    se ve prod dinamico

--- Etapa3/supermarket-front/src/components/product-detail/product-detail.jsx
+++ Etapa3/supermarket-front/src/components/product-detail/product-detail.jsx
@@ -15,61 +15,67 @@ class ProductDetail extends Component {
         var id = this.props.match.params.id
         var self = this
         if (id) {
-            console.log('http://localhost:3010/api/products/' + id)
             axios.get('http://localhost:3010/api/products/' + id)
                 .then(response => {
-                    console.log(response.data)
-                    self.setState({ loadedProduct: response.data });
+                    self.setState({ loadedProduct: response.data.data });
                 }).catch(function (error) {
                     console.log(error);
-                  })  
+                })  
         }
     }
 
     render() {
+        let product = ""
+        if(this.state.loadedProduct){
+            var prod = this.state.loadedProduct
 
-        return(<div className="container">
-                <div className="card text-center">
-                    <div className="card-header">
-                        ¡Terminá tu compra!
-                </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-sm-12 col-md-4">
-                                <div class="product-detail-data">
-                                    <h3 className="card-title capitalize">{this.props.name}</h3>
-                                    <h6 className="card-subtitle mb-2 text-muted capitalize">{this.props.type}</h6>
-                                    <p className="card-text product-price-size">$ {this.props.price}</p>
-                                </div>
-                            </div>
-                            <div className="col-sm-12 col-md-8">
-                                <form>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-6">
-                                            <div className="form-group">
-                                                <label for="cantidadProductos">Cantidad de productos</label>
-                                                <input type="number" className="form-control" id="cantidadProductos" placeholder="Ingrese la cantidad de productos" required min="1" defaultValue="1"/>
-                                            </div>
-                                            <div className="form-group">
-                                                <label for="cupon">Número de cupón</label>
-                                                <input type="Number" className="form-control" id="cupon" placeholder="Ingrese el número de su cupón" />
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-12 col-md-6">
-                                            <button type="submit" className="btn btn-primary buyButton">Comprar</button>
-                                        </div>
-                                    </div>
-                                </form>
+            product = (<div className="container">
+            <div className="card text-center">
+                <div className="card-header">
+                    ¡Terminá tu compra!
+            </div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-4">
+                            <div className="product-detail-data">
+                                <h3 className="card-title capitalize">{prod.name}</h3>
+                                <h6 className="card-subtitle mb-2 text-muted capitalize">{prod.producttype}</h6>
+                                <p className="card-text product-price-size">$ {prod.saleprice}</p>
                             </div>
                         </div>
-                        <div className="card-footer text-muted">
-                            Recibirás el producto en los próximos días
+                        <div className="col-sm-12 col-md-8">
+                            <form>
+                                <div className="row">
+                                    <div className="col-sm-12 col-md-6">
+                                        <div className="form-group">
+                                            <label>Cantidad de productos</label>
+                                            <input type="number" className="form-control" id="cantidadProductos" placeholder="Ingrese la cantidad de productos" required min="1" defaultValue="1"/>
+                                        </div>
+                                        <div className="form-group">
+                                            <label>Número de cupón</label>
+                                            <input type="Number" className="form-control" id="cupon" placeholder="Ingrese el número de su cupón" />
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-12 col-md-6">
+                                        <button type="submit" className="btn btn-primary buyButton">Comprar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    </div>
+                    <div className="card-footer text-muted">
+                        Recibirás el producto en los próximos días
+                </div>
                 </div>
             </div>
-        );
+        </div>
+    )
+
     }
+    
+    return product;
+    
+}
 }
 
 export default ProductDetail;
