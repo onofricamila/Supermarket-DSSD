01eb5ba789de9da4e282158ed8a71863c36bd273
Franco Liptak <franco.liptak@despegar.com>
2018-11-26 19:20:16 -0300

    modificaciones en cuadro de compra

--- Etapa3/supermarket-front/src/components/product-detail/product-detail.jsx
+++ Etapa3/supermarket-front/src/components/product-detail/product-detail.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './product-detail.css';
 import axios from 'axios'
-import {Button} from 'reactstrap'
-import {AuthContext} from '../../App';
-import {Redirect} from 'react-router-dom'
+import { Button } from 'reactstrap'
+import { AuthContext } from '../../App';
+import { Redirect } from 'react-router-dom'
 
 class ProductDetail extends Component {
     state = {
@@ -12,54 +12,54 @@ class ProductDetail extends Component {
             cant: 1,
             couponNumber: ''
         },
-        msj:'¡Terminá tu compra!',
+        msj: '¡Terminá tu compra!',
         extraDiscount: 1,
         isEmployee: false
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
-    componentWillMount(){
+    componentWillMount() {
         var id = this.props.match.params.id
         var self = this
         if (id) {
-            axios.get('http://localhost:3003/products/' + id, {headers: {'token': this.props.auth}})
+            axios.get('http://localhost:3003/products/' + id, { headers: { 'token': this.props.auth } })
                 .then(response => {
                     console.log(response)
-                    let currentState = self.state 
-                    let currentForm = self.state.form 
-                    self.setState({ 
+                    let currentState = self.state
+                    let currentForm = self.state.form
+                    self.setState({
                         ...currentState,
-                        form: {...currentForm},
+                        form: { ...currentForm },
                         loadedProduct: response.data.data,
                     });
                 }).catch(function (error) {
                     console.log(error);
-                })  
+                })
 
-          
-            axios.get('http://localhost:3003/isEmployee/', {headers: {'token': this.props.auth}})
-            .then(response => {
-                console.log(response)
-                let currentState = self.state 
-                let currentForm = self.state.form 
-                self.setState({ 
-                    ...currentState,
-                    form: {...currentForm},
-                    isEmployee: true
-                });
-            }).catch(function (error) {
-                console.log(error);
-                let currentState = self.state 
-                let currentForm = self.state.form 
-                self.setState({ 
-                    ...currentState,
-                    form: {...currentForm},
-                    isEmployee: false
-                })  
-            })  
+
+            axios.get('http://localhost:3003/isEmployee/', { headers: { 'token': this.props.auth } })
+                .then(response => {
+                    console.log(response)
+                    let currentState = self.state
+                    let currentForm = self.state.form
+                    self.setState({
+                        ...currentState,
+                        form: { ...currentForm },
+                        isEmployee: true
+                    });
+                }).catch(function (error) {
+                    console.log(error);
+                    let currentState = self.state
+                    let currentForm = self.state.form
+                    self.setState({
+                        ...currentState,
+                        form: { ...currentForm },
+                        isEmployee: false
+                    })
+                })
         }
     }
 
@@ -67,47 +67,47 @@ class ProductDetail extends Component {
         let currentState = this.state
         let currentForm = this.state.form
         this.setState({
-          ...currentState,
-          form: {
-              ...currentForm,
-              [name]: event.target.value,
-          }
+            ...currentState,
+            form: {
+                ...currentForm,
+                [name]: event.target.value,
+            }
         })
     }
 
-    isEmpty(field){
+    isEmpty(field) {
         return field.length == 0
     }
 
-    hasOnlyDigits(field){
+    hasOnlyDigits(field) {
         var re = /^[0-9]*$/;
-        return (re.test(field)) 
+        return (re.test(field))
     }
 
-    validCant(form){
-        let currentState = this.state 
+    validCant(form) {
+        let currentState = this.state
         let loadedProduct = this.state.loadedProduct
-        if(this.isEmpty(form.cant)){
+        if (this.isEmpty(form.cant)) {
             this.setState({
                 ...currentState,
-                loadedProduct: {...loadedProduct},
+                loadedProduct: { ...loadedProduct },
                 form: form,
                 msj: '¡Debes ingresar la cantidad!',
             })
             return false
         }
 
-        if (!this.hasOnlyDigits(form.cant)){
+        if (!this.hasOnlyDigits(form.cant)) {
             this.setState({
                 ...currentState,
-                loadedProduct: {...loadedProduct},
+                loadedProduct: { ...loadedProduct },
                 form: form,
                 msj: '¡Debes ingresar solo números en el campo "Cantidad"!',
             })
             return false
         }
 
-        if(loadedProduct.stock - form.cant < 0){
+        if (loadedProduct.stock - form.cant < 0) {
             this.setState({
                 ...currentState,
                 msj: '¡Disminuye la cantidad! Supera al stock en "' + (loadedProduct.stock - form.cant) * -1 + '" unidad/es.'
@@ -118,26 +118,26 @@ class ProductDetail extends Component {
         return true
     }
 
-    async validCoupon(form){
+    async validCoupon(form) {
         let loadedProduct = this.state.loadedProduct
-        let currentState = this.state 
-        
+        let currentState = this.state
+
         // esta vacio el campo del cupon, es correcto
-        if(this.isEmpty(form.couponNumber)){ 
+        if (this.isEmpty(form.couponNumber)) {
             this.setState({
                 ...currentState,
-                loadedProduct: {...loadedProduct},
+                loadedProduct: { ...loadedProduct },
                 form: form,
                 extraDiscount: 1
             })
             return true
         }
-        
+
         // si esta completo el campo y no tiene solo digitos, es incorrecto
-        if(!this.hasOnlyDigits(form.couponNumber)){
+        if (!this.hasOnlyDigits(form.couponNumber)) {
             this.setState({
                 ...currentState,
-                loadedProduct: {...loadedProduct},
+                loadedProduct: { ...loadedProduct },
                 form: form,
                 msj: '¡Debes ingresar solo números para el campo "Número de cupón"!',
             })
@@ -147,111 +147,111 @@ class ProductDetail extends Component {
         // sabemos se lleno el campo y se ingresó un numero, veremos si corresponde a un cupon
         var self = this
         var action = await axios.get(`http://localhost:3003/coupon/${form.couponNumber}`)
-        .then(function (response) {
-            console.log(response)
-            // no existe ese numero de cupon
-            if(response.data.status == "resource not found"){
+            .then(function (response) {
+                console.log(response)
+                // no existe ese numero de cupon
+                if (response.data.status == "resource not found") {
+                    self.setState({
+                        ...currentState,
+                        loadedProduct: { ...loadedProduct },
+                        form: form,
+                        msj: '¡Número de cupón inválido!',
+                    })
+                    return false
+                }
+
+                // el cupon existe pero ya fue usado
+                if (response.data.data.used == 1) {
+                    self.setState({
+                        ...currentState,
+                        loadedProduct: { ...loadedProduct },
+                        form: form,
+                        msj: '¡Ese cupón ya fue utilizado!',
+                    })
+                    return false
+                }
+
+                // existe el cupon y se puede usar
+                self.setState({
+                    loadedProduct: { ...loadedProduct },
+                    form: form,
+                    msj: 'Cupon valido',
+                    extraDiscount: 1 - parseInt(response.data.data.discount_percentage) / 100
+                })
+                return true
+
+            })
+            .catch(function (error) {
+                console.log(error);
                 self.setState({
                     ...currentState,
-                    loadedProduct: {...loadedProduct},
+                    loadedProduct: { ...loadedProduct },
                     form: form,
-                    msj: '¡Número de cupón inválido!',
-                }) 
+                    msj: 'Oops, hubo un problema :(',
+                })
                 return false
-            }
+            })
 
-            // el cupon existe pero ya fue usado
-            if(response.data.data.used == 1){
-                self.setState({
-                    ...currentState,
-                    loadedProduct: {...loadedProduct},
-                    form: form,
-                    msj: '¡Ese cupón ya fue utilizado!',
-                }) 
-                return false
-            }
-
-            // existe el cupon y se puede usar
-            self.setState({
-                loadedProduct: {...loadedProduct},
-                form: form,
-                msj: 'Cupon valido',
-                extraDiscount: 1-parseInt(response.data.data.discount_percentage)/100
-            }) 
-            return true
-            
-        })
-        .catch(function (error) {
-            console.log(error);
-            self.setState({
-                ...currentState,
-                loadedProduct: {...loadedProduct},
-                form: form,
-                msj: 'Oops, hubo un problema :(',
-            })      
-            return false
-        }) 
-        
         console.log('lo de axios ' + action)
         return action
     }
 
-    async canSubmit(){
+    async canSubmit() {
         let form = this.state.form
         return (this.validCant(form) && this.validCoupon(form))
     }
 
-    purchase(){
+    purchase() {
         var self = this;
         let form = this.state.form
         let loadedProduct = this.state.loadedProduct
         let currentState = this.state
-       
+
         let params = {
             productid: loadedProduct.id,
             quantity: form.cant
         }
-        
-        if(!this.isEmpty(currentState.form.couponNumber)){
-            params = { 
+
+        if (!this.isEmpty(currentState.form.couponNumber)) {
+            params = {
                 ...params,
                 coupon: form.couponNumber
             }
         }
-     
-        axios.post(`http://localhost:3003/buy`, params, {headers: {'token': this.props.auth}})
-          .then(function (response) {
-            console.log(response);
-            self.setState({
-                ...currentState,
-                loadedProduct: {...loadedProduct, stock: loadedProduct.stock-form.cant},
-                form: form,
-                msj: 'Recibirás el producto en los próximos días :)',
+
+        axios.post(`http://localhost:3003/buy`, params, { headers: { 'token': this.props.auth } })
+            .then(function (response) {
+                console.log(response);
+                self.setState({
+                    ...currentState,
+                    loadedProduct: { ...loadedProduct, stock: loadedProduct.stock - form.cant },
+                    form: form,
+                    msj: 'Recibirás el producto en los próximos días :)',
+                })
+                if (loadedProduct.stock - form.cant == 0) {
+                    self.props.onBuy(loadedProduct.id)
+                    self.props.history.push('/')
+                }
+
             })
-            if(loadedProduct.stock - form.cant == 0){
-                self.props.onBuy(loadedProduct.id)
-                self.props.history.push('/')
-            }
-            
-          })
-          .catch(function (error) {
-            console.log(error);
-            self.setState({
-                ...currentState,
-                loadedProduct: {...loadedProduct},
-                form: form,
-                msj: 'Algo salio mal :(',
+            .catch(function (error) {
+                console.log(error);
+                self.setState({
+                    ...currentState,
+                    loadedProduct: { ...loadedProduct },
+                    form: form,
+                    msj: 'Algo salio mal :(',
+                })
             })
-          }) 
     }
 
     submit = () => {
         var self = this;
 
-        this.canSubmit().then(result =>{ 
-            console.log('Can Submit: ', result) 
+        this.canSubmit().then(result => {
+            console.log('Can Submit: ', result)
             // codigo con result 
-            if(result){
+            if (result) {
                 let currentState = self.state
                 let cant = self.state.form.cant
                 let extraDiscount = self.state.extraDiscount
@@ -263,8 +263,9 @@ class ProductDetail extends Component {
                     self.purchase()
                 } else {
                     self.setState(
-                        {...currentState,
-                        msj: 'Optaste por cancelar... ¿estás seguro que no quieres el producto?'
+                        {
+                            ...currentState,
+                            msj: 'Optaste por cancelar... ¿estás seguro que no quieres el producto?'
                         }
                     )
                 }
@@ -272,74 +273,71 @@ class ProductDetail extends Component {
         })
     }
 
-    showFinalPrice(){
+    showFinalPrice() {
         var prod = this.state.loadedProduct
         var cant = this.state.form.cant
-        if(this.hasOnlyDigits(cant)){
-            return  this.roundedMultiplicationOf([prod.price, cant])
+        if (this.hasOnlyDigits(cant)) {
+            return this.roundedMultiplicationOf([prod.price, cant])
         }
         return '---'
     }
 
-    roundedMultiplicationOf(factors){
-        return Math.round(factors.reduce( (a,b) => a * b )*100)/100
+    roundedMultiplicationOf(factors) {
+        return Math.round(factors.reduce((a, b) => a * b) * 100) / 100
     }
 
     render() {
-        if (this.state.redirect == true || !this.props.auth)  return (<Redirect to='/' />) 
+        if (this.state.redirect == true || !this.props.auth) return (<Redirect to='/' />)
 
         let show = ""
-        if(this.state.loadedProduct){
+        if (this.state.loadedProduct) {
             var prod = this.state.loadedProduct
 
-            show = (<div className="container">
-            <div className="card text-center">
-                <div className="card-header">
-                       Accediste a comprar:
-            </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-4">
-                            <div className="product-detail-data">
-                                <h3 className="card-title capitalize">{prod.name}</h3>
-                                <h6 className="card-subtitle mb-2 text-muted capitalize">{prod.type.description}</h6>
-                                <p className="card-text product-price-size">$ {this.showFinalPrice()}</p>
-                            </div>
+            show = (
+                <div className="container product-detail-container">
+                    <div className="card text-center">
+                        <div className="card-header">
+                            Accediste a comprar:
                         </div>
-                        <div className="col-sm-12 col-md-8">
+                        <div className="card-body">
                             <form>
                                 <div className="row">
                                     <div className="col-sm-12 col-md-6">
-                                        <div className="form-group">
-                                            <label>Cantidad:</label>
-                                            <input onChange={this.handleChange('cant')} className="form-control" id="cantidadProductos" placeholder="Ingrese la cantidad de productos" value={this.state.form.cant}/>
+                                        <div className="product-detail-data">
+                                            <h3 className="card-title capitalize">{prod.name}</h3>
+                                            <h6 className="card-subtitle mb-2 text-muted capitalize">{prod.type.description}</h6>
+                                            <p className="card-text product-price-size">$ {this.showFinalPrice()}</p>
                                         </div>
-
-                                            {this.state.isEmployee ? '' : <div className="form-group">
-                                                <label>Número de cupón (opcional): </label>
-                                                <input onChange={this.handleChange('couponNumber')}  className="form-control" id="cupon" placeholder="Ingrese el número de su cupón" value={this.state.form.couponNumber} />
-                                            </div> }
                                     </div>
                                     <div className="col-sm-12 col-md-6">
-                                        <Button className='buyButton' onClick={this.submit}>Comprar</Button>
+                                        <div className="form-group">
+                                            <label>Cantidad:</label>
+                                            <input onChange={this.handleChange('cant')} className="form-control" id="cantidadProductos" placeholder="Ingrese la cantidad de productos" value={this.state.form.cant} />
+                                        </div>
+
+                                        {this.state.isEmployee ? '' : <div className="form-group">
+                                            <label>Número de cupón (opcional): </label>
+                                            <input onChange={this.handleChange('couponNumber')} className="form-control" id="cupon" placeholder="Ingrese el número de su cupón" value={this.state.form.couponNumber} />
+                                        </div>}
                                     </div>
                                 </div>
+                                <div className="col-sm-12">
+                                    <Button className='buyButton btn-lg' onClick={this.submit}>Comprar</Button>
+                                </div>
                             </form>
+                            <div className="card-footer text-muted">
+                                {this.state.msj}
+                            </div>
                         </div>
                     </div>
-                    <div className="card-footer text-muted">
-                         {this.state.msj}
                 </div>
-                </div>
-            </div>
-        </div>
-    )
+            )
+
+        }
+
+        return show;
 
     }
-    
-    return show;
-    
-}
 }
 
 export default ProductDetail;
